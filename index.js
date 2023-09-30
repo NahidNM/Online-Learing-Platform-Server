6b96df8efca3fbbbb7ec5b19f053569c6822f555
NahidNM <mdnahidhasan171@gmail.com>
2023-09-30 20:13:05 +0600

    jwt added

--- index.js
+++ index.js
@@ -4,13 +4,36 @@ require('dotenv').config()
 const cors = require('cors');
 const app = express();
 
-const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
+
+const port = process.env.PORT || 4000;
 
 // midleware
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
+
+// token verifie
+const verifyJWT = (req, res, next) =>{
+  const authorization =req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'})
+  }
+
+
+ // bearer token
+ const token = authorization.split(' ')[1];
+
+ jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) =>{
+   if(error){
+     return res.status(401).send({error: true, message: 'unauthorized access'})
+   }
+req.decoded= decoded;
+next();
+ })
+} 
+
+// console.log(process.env.DB_USER);
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jo7sbx1.mongodb.net/?retryWrites=true&w=majority`;
@@ -29,15 +52,73 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // -------Mongobd Added database---------
+
     const allLearningCourses = client.db('OnlineLearningPlatform').collection('course');
+    const addCourseCollection = client.db("OnlineLearningPlatform").collection("addCartCourse");
+    const usersCollection = client.db("OnlineLearningPlatform").collection("LearningUsers");
+
+
+    // -----------jWT--------
+
+    app.post('/jwt', (req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' })
+     res.send({token})
+    })
 
 
     app.get('/course', async(req, res) =>{
         const cursor = allLearningCourses.find();
         const result = await cursor.toArray();
-        console.log(result)
+        // console.log(result)
         res.send(result)
       })
+
+
+      // user add to Cart
+
+      app.get('/cart', verifyJWT, async(req, res) =>{
+        const email = req.query.email;
+        // console.log(email);
+        if(!email){
+         return res.send([])
+        }
+      
+        const decodedEmail = req.decoded.email;
+        if(email !== decodedEmail){
+          return res.status(401).send({error: true, message: 'forbiden access'})
+        }
+      
+        const query = {email: email}
+        const result = await addCourseCollection.find(query).toArray();
+        res.send(result)
+      })
+
+      app.post('/cart', async (req, res) => {
+        const item = req.body;
+        const result = await addCourseCollection.insertOne(item);
+        res.send(result);
+      })
+
+
+// ----------- user releted database------
+
+app.post('/users', async(req, res)=>{
+  const user = req.body;
+// console.log(user);
+const quary = {email: user.email}
+const existingUser = await usersCollection.findOne(quary);
+// console.log(existingUser);
+if(existingUser){
+  return res.send({message: 'user already exists'})
+}
+ const result = await usersCollection.insertOne(user);
+  res.send(result)
+})
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
